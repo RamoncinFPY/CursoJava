fc3f556e664e689bf1499a399a3e6205563b1f5a
Ramón <ramonfportyer@hotmail.com>
2022-04-27 21:30:04 +0200

    Update OperacionesBasicasMain.java

--- operacionesBasicas/OperacionesBasicasMain.java
+++ operacionesBasicas/OperacionesBasicasMain.java
@@ -3,7 +3,7 @@ package operacionesBasicas;
 public class OperacionesBasicasMain {
     public static void main(String[] args) {
         OperacionesBasicas calculadora= new OperacionesBasicas();
-        calculadora.calculo();
+        
         calculadora.toString();
     }
 }
