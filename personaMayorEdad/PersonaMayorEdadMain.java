5f235c23088e98a58ca89b463994a02d0a175c15
Ramón <ramonfportyer@hotmail.com>
2022-04-29 11:50:40 +0200

    Delete personaMayorEdad directory

--- personaMayorEdad/PersonaMayorEdadMain.java
+++
@@ -1,8 +0,0 @@
-package personaMayorEdad;
-
-public class PersonaMayorEdadMain {
-    public static void main(String[] args) {
-        PersonaMayorEdad persona1 = new PersonaMayorEdad();
-        persona1.toString();
-    }
-}
