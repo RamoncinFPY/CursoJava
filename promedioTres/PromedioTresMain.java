560cd1fee8c3cdd8c3500d33b15aa2171b36d6ed
Ramón <ramonfportyer@hotmail.com>
2022-05-03 16:36:53 +0200

    Update PromedioTresMain.java

--- promedioTres/PromedioTresMain.java
+++ promedioTres/PromedioTresMain.java
@@ -3,8 +3,6 @@ package promedioTres;
 public class PromedioTresMain {
     public static void main(String[] args) {
         PromedioTresCalculos p1 = new PromedioTresCalculos();
-        p1.totalTres();
-        p1.promedioTres();
         System.out.println(p1.salida());
     }
 }
